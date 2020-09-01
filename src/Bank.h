6eb62afd233aa1cb57969b4c98be8c9587279694
Stephanie Vargas <vstephanie774@gmail.com>
2020-09-01 09:03:49 -0600

    Bank

--- src/Bank.h
+++ src/Bank.h
@@ -4,16 +4,15 @@
 
 #ifndef LAB_05_BANK_H
 #define LAB_05_BANK_H
+#include<sstream>
 
-
+using namespace std;
 class Bank {
 
-
-
-
-
-
-
+public:
+   virtual string processPaymentBankTransfer() = 0;
+   virtual string processPaymentCash() = 0;
+   virtual string processPaymentCheck()= 0;
 
 };
 
