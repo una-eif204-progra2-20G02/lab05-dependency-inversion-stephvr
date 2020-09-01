b0865c7642ddce40dc68598b3e0341b44601d5c9
Stephanie Vargas <vstephanie774@gmail.com>
2020-09-01 09:57:18 -0600

    Bank

--- src/LocalBank.cpp
+++ src/LocalBank.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "LocalBank.h"
+
+std::string LocalBank::processPaymentBankTransfer() {
+    BankTransferSender bankTransferSender;
+    return bankTransferSender.sendPayment();
+}
+
+std::string LocalBank::processPaymentCash() {
+    CashSender cashSender;
+    return cashSender.sendPayment();
+}
+
+std::string LocalBank::processPaymentCheck() {
+    CheckSender checkSender;
+    return checkSender.sendPayment();
+}
--- src/LocalBank.h
+++ src/LocalBank.h
@@ -4,15 +4,20 @@
 
 #ifndef LAB_05_LOCALBANK_H
 #define LAB_05_LOCALBANK_H
+#include "BankTransferSender.h"
+#include "CashSender.h"
+#include "CheckSender.h"
 
+using namespace std;
 
 class LocalBank
 {
 
 public:
 
-
-
+    std::string processPaymentBankTransfer();
+    std::string processPaymentCash();
+    std::string processPaymentCheck();
 
 
 };
